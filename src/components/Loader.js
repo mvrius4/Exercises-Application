e2623341cdf1df14095de4ca67d0f81b20797793
mvrius69 <xmariusro@gmail.com>
2022-07-25 11:04:49 +0300

    loader

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { InfinitySpin } from 'react-loader-spinner';
+
+const Loader = () => {
+    return (
+        <div className='loader'>
+            <InfinitySpin />
+        </div>
+    )
+}
+
+export default Loader
